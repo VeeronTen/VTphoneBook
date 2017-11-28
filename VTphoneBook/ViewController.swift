4b496e3a1d1baa43337ecdfefd07568a10caac11
Veeron Ten <veeronten@Veerons-iMac.local>
2017-11-28 23:29:38 +0300

    Initial Commit

---
+++ VTphoneBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VTphoneBook
+//
+//  Created by Veeron Ten on 11/28/17.
+//  Copyright © 2017 Veeron Ten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
